7ea1515495e3b5fa623bec69c2e8fd7b4127f5bf
BingKui <178974662@qq.com>
2020-08-14 15:40:53 +0800

    添加基本展示

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,20 +1,38 @@
 <template>
-    <div>
-        首页 index
-        <Button>按钮</Button>
+    <div class="v-index-page">
+        <Divider orientation="center">示例项目</Divider>
+        <div class="desc">本项目为示例项目，主要包含基本的框架，打包封装。可以零基础直接进行插件的开发。内置包含 "ant-design-vue" 组件库，方便快速构建界面。</div>
+        <Divider orientation="left">AntdV基础组件展示</Divider>
+        <div class="components-list">
+            <Button>按钮</Button>
+            <Icon type="smile" theme="twoTone" style="fontSize: 25px;" />
+        </div>
     </div>
 </template>
 
 <script>
-import { Button } from 'ant-design-vue';
+import { Button, Divider, Icon } from 'ant-design-vue';
 export default {
     name: 'Index',
     components: {
         Button,
+        Divider,
+        Icon,
     },
 };
 </script>
 
 <style lang="less">
-
+.v-index-page {
+    .p(@gap-md);
+    .desc {
+        .font-size();
+        color: @text-color;
+        text-indent: @font-size * 2;
+    }
+    .components-list {
+        .flex();
+        align-items: center;
+    }
+}
 </style>
