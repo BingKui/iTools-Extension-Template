7d5cb2013326d5ec8a0eb9a130fc963acce9054c
BingKui <178974662@qq.com>
2020-08-15 11:29:23 +0800

    增加自定义滚动条

--- src/index.vue
+++ src/index.vue
@@ -1,9 +1,15 @@
 <template>
-    <router-view />
+    <ScrollBar class="v-main-scroll">
+        <router-view />
+    </ScrollBar>
 </template>
 <script>
 import '@styles/main.less';
+import ScrollBar from '@components/ScrollBar';
 export default {
     name: 'index',
+    components: {
+        ScrollBar,
+    },
 };
 </script>
